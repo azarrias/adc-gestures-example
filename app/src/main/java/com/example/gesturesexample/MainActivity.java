82bb039a91e93b10fda5c4518c17f68a9635e25c
azarrias <adolfo.zarrias@gmail.com>
2019-04-20 00:34:45 +0200

    Make predictions for each gesture comparing to those in the library (A to G letters)

--- app/src/main/java/com/example/gesturesexample/MainActivity.java
+++ app/src/main/java/com/example/gesturesexample/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.gesturesexample;
 
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.Prediction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements GestureOverlayView.OnGesturePerformedListener {
+
+    private GestureLibrary gestureLibrary;
+    private TextView tvOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gestureLibrary = GestureLibraries.fromRawResource(this, R.raw.gestures);
+        if (!gestureLibrary.load()) {
+            finish();
+        }
+        GestureOverlayView gesturesView = findViewById(R.id.gestures);
+        gesturesView.addOnGesturePerformedListener(this);
+        tvOutput = findViewById(R.id.tv_output);
+    }
+
+    @Override
+    public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+        ArrayList<Prediction> predictions = gestureLibrary.recognize(gesture);
+        tvOutput.setText("");
+        for (Prediction prediction : predictions) {
+            tvOutput.append("prediction: " + prediction.name +
+                    " score: " + prediction.score + "\n");
+        }
     }
 }
